f74129b8329c25ee9bb5f9468c5acd926faee638
Sidner <sidner@student.chalmers.se>
2018-11-03 11:51:53 +0100

    added basic sound, envelope and filter functionality

--- synth-one/Source/SynthVoice.h
+++ synth-one/Source/SynthVoice.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
   ==============================================================================
 
     SynthVoice.h
@@ -10,6 +10,7 @@
 
 #pragma once
 #include "../JuceLibraryCode/JuceHeader.h"
+#include "Maximilian.h"
 #include "SynthSound.h"
 #include <iostream>
 #include <string>
@@ -24,18 +25,45 @@ public:
 
 	void startNote(int midiNoteNumber, float velocity, SynthesiserSound * sound, int currentPitchWheelPosition)
 	{
+		env1.trigger = 1;
+
+		level = velocity;
 		frequency = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
 		Logger::outputDebugString(std::to_string(midiNoteNumber));
 	}
 
 	void stopNote(float velocity, bool allowTailOff)
 	{
-		clearCurrentNote();
+		env1.trigger = 0;
+		allowTailOff = true;
+
+		if (velocity = 0)
+		{
+			clearCurrentNote();
+		}
 	}
 
 	void renderNextBlock(AudioBuffer<float> &outputBuffer, int startSample, int numSamples)
 	{
+		// set adsr for voice
+		env1.setAttack(10);
+		env1.setDecay(500);
+		env1.setSustain(0.8);
+		env1.setRelease(200);
 
+		for (int sample = 0; sample < numSamples; ++sample)
+		{
+			// create sound wave to output
+			float wave = osc1.saw(frequency);
+			float sound = env1.adsr(wave, env1.trigger) * level;
+			float filteredSound = filter1.lores(sound, 3000, 0.5);
+
+			for (int channel = 0; channel < outputBuffer.getNumChannels(); ++channel)
+			{
+				outputBuffer.addSample(channel, startSample, filteredSound);
+			}
+			++startSample;
+		}
 	}
 
 	void pitchWheelMoved(int newPitchWheelValue)
@@ -51,4 +79,8 @@ public:
 private:
 	double level;
 	double frequency;
+
+	maxiOsc osc1;
+	maxiEnv env1;
+	maxiFilter filter1;
 };
